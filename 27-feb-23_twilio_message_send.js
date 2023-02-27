f7a5661609b2cf764c1675104cce208bfd0dd37d
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-02-27 11:49:02 +0530

    Twilio Whatsapp message sender updated

---
+++ 27-feb-23_twilio_message_send.js
@@ -0,0 +1,21 @@
+/**
+ * Author :- Sandip Nandi
+ * Creation Date :- 27-Feb-23
+ * Address :- Bangalore, Karnataka
+ **/
+
+
+
+
+// npm install twilio
+const twilio = require('twilio');
+
+const accountSid = 'ACc40eb1c0368aca104f3f07a3ddb223dd';
+const authToken = '464dac8466082d9e85d2307aef1d62b1';
+
+const client = new twilio(accountSid, authToken);
+client.messages.create({
+  from: 'whatsapp:+14155238886',  
+  body: 'Your Twilio code is 1238432',      
+  to: 'whatsapp:+4915128288055' 
+}).then(msg => console.log(msg.sid));
