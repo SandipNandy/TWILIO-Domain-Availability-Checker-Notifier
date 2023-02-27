676ee817f9e5f213648e6e26e97fb42dc67c8ac3
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-02-27 11:50:55 +0530

    twilio whatsapp message sender project uploaded

---
+++ 27-feb-23_twilio_message_send.py
@@ -0,0 +1,21 @@
+#############################################
+##Author :- Sandip Nandi                    #
+##Creation Date :- 27- feb-23               #
+##Address :- Bengalore, Karanataka          #
+#############################################
+
+
+
+from twilio.rest import Client 
+ 
+account_sid = 'ACc40eb1368aca104f3f07a3ddb223dd' 
+auth_token = '464dac846608285d2307aef1d62b1' 
+client = Client(account_sid, auth_token) 
+ 
+message = client.messages.create( 
+                              from_='whatsapp:+14XXXXX',  
+                              body='Your Twilio code is 13843',      
+                              to='whatsapp:+91XXXXXXXX' 
+                          ) 
+ 
+print(message.sid)
