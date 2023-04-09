461c52f42f56762db5ae29bd3460f9444096a73c
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-04-09 10:05:58 +0530

    Update 27-feb-23_twilio_message_send.py

--- 27-feb-23_twilio_message_send.py
+++ 27-feb-23_twilio_message_send.py
@@ -2,7 +2,7 @@
 ##Author :- Sandip Nandi                      #
 ##Creation Date :- 27- feb-23                 #
 ##Address :- Bengalore, Karanataka            #
-##Domain Availability Checker Notifier        #
+## Domain Availability Checker Notifier       #
 ###############################################
 
 
