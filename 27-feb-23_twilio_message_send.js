7778bf1f77ebfc3fc4d5a95f61edcfc01f950716
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-03 12:17:48 +0530

    Update 27-feb-23_twilio_message_send.js

--- 27-feb-23_twilio_message_send.js
+++ 27-feb-23_twilio_message_send.js
@@ -6,8 +6,11 @@
  * Availability Checker Notifier.
  **/
 
+
+
 // npm install twilio
 const twilio = require('twilio');
+
 const accountSid = 'ACc40eb1c0368aca104f3f07a3ddb223dd';
 const authToken = '464dac8466082d9e85d2307aef1d62b1';
 const client = new twilio(accountSid, authToken);
@@ -16,6 +19,5 @@ client.messages.create({
   body: 'Your Twilio code is 1238432',      
   to: 'whatsapp:+4915128288055' 
 }).then(msg => console.log(msg.sid));
-
 
 //   twilio message send.
