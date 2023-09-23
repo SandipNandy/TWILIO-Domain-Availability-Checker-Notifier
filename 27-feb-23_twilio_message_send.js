ad17ea962cd08566aab5dfd35eab14d19dd33c7d
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-23 19:18:37 +0530

    Update 27-feb-23_twilio_message_send.js

--- 27-feb-23_twilio_message_send.js
+++ 27-feb-23_twilio_message_send.js
@@ -10,7 +10,6 @@
 
 const twilio = require('twilio');
 const accountSid = 'ACc40eb1c0368aca104f3f07a3ddb223dd';
-
 const authToken = '464dac8466082d9e85d2307aef1d62b1';
 
 const client = new twilio(accountSid, authToken);
