d3cd58df8dc8654cb87279970fbfcd0590660447
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-10-05 09:02:08 +0530

    Update 27-feb-23_Twilio_domain_checker.py

--- 27-feb-23_Twilio_domain_checker.py
+++ 27-feb-23_Twilio_domain_checker.py
@@ -15,8 +15,8 @@ from twilio.rest import Client
 parser = argparse.ArgumentParser(description="Check domain for availability")
 parser.add_argument("domain", type=str, help="Domain name to be checked")
 args = parser.parse_args()
-# godaddy API credentials for authorization
 
+# godaddy API credentials for authorization
 api_key = "3mM44UahMcshjE_HUPueqAPN6dU6DSVoBAsxn"
 api_secret = "QdV9FjZjKvxXmVFqfLJEMk"
 req_headers = {
