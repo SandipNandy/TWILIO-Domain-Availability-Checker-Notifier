519d9e764889f6d5bf1466dcb5a777b2e2268a44
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-10-05 09:02:50 +0530

    Update 27-feb-23_twilio_message_send.js

--- 27-feb-23_twilio_message_send.js
+++ 27-feb-23_twilio_message_send.js
@@ -13,7 +13,8 @@ const authToken = '464dac8466082d9e85d2307aef1d62b1';
 const client = new twilio(accountSid, authToken);
 
 client.messages.create({
-  from: 'whatsapp:+14155238886',  
+  from: 'whatsapp:+14155238886', 
+  
   body: 'Your Twilio code is 1238432', 
   to: 'whatsapp:+4915128288055' 
 }).then(msg => console.log(msg.sid));
