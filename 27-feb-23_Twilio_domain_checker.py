7ba259818042fe29cf2b2eaf6d8be80754b34629
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-12-28 08:54:17 +0530

    Update 27-feb-23_Twilio_domain_checker.py

--- 27-feb-23_Twilio_domain_checker.py
+++ 27-feb-23_Twilio_domain_checker.py
@@ -17,6 +17,7 @@ from twilio.rest import Client
 
 # set up cli domain argument
 parser = argparse.ArgumentParser(description="Check domain for availability")
+
 parser.add_argument("domain", type=str, help="Domain name to be checked")
 args = parser.parse_args()
 
