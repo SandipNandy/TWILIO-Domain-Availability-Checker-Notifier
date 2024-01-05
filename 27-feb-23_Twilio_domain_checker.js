af87a7807cba535de88965c670ad7ac4e3a8201e
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-01-05 08:11:22 +0530

    Update 27-feb-23_Twilio_domain_checker.js

--- 27-feb-23_Twilio_domain_checker.js
+++ 27-feb-23_Twilio_domain_checker.js
@@ -13,7 +13,6 @@ const axios = require('axios').default;
 const { ArgumentParser } = require('argparse');
 const parser = new ArgumentParser({ description: 'Check domain for availability' });
 parser.addArgument('domain', {
-  
   type: 'string',
   
   help: 'Domain name to be checked',
