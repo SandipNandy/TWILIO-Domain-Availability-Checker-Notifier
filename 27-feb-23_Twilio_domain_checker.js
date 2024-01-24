7c28a686ed606b1c799c06da601225324331f7dd
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-01-24 09:39:07 +0530

    Update 27-feb-23_Twilio_domain_checker.js

--- 27-feb-23_Twilio_domain_checker.js
+++ 27-feb-23_Twilio_domain_checker.js
@@ -29,6 +29,7 @@ const accountSid = 'ENTER ACCOUNT SID HERE';
 const authToken = 'ENTER AUTH TOKEN HERE';
 
 const whatsappNumber = 'ENTER NUMBER HERE';
+
 const client = new twilio(accountSid, authToken);
 
 // godaddy credentials
