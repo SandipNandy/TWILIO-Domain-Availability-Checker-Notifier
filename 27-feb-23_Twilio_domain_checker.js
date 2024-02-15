b60d65ab8e45137610a63eb5e88c941f78c4e83c
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-02-16 02:30:52 +0530

    Update 27-feb-23_Twilio_domain_checker.js

--- 27-feb-23_Twilio_domain_checker.js
+++ 27-feb-23_Twilio_domain_checker.js
@@ -20,7 +20,6 @@ const { domain } = parser.parseArgs();
 
 // twilio credentials
 const accountSid = 'ENTER ACCOUNT SID HERE';
-
 const authToken = 'ENTER AUTH TOKEN HERE';
 
 const whatsappNumber = 'ENTER NUMBER HERE';
