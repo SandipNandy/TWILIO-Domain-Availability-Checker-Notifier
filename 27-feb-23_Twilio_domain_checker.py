12d41374e334b0c559530af32a2900d58fd73dbe
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-06-23 12:09:31 +0530

    Update 27-feb-23_Twilio_domain_checker.py

--- 27-feb-23_Twilio_domain_checker.py
+++ 27-feb-23_Twilio_domain_checker.py
@@ -17,6 +17,7 @@ parser.add_argument("domain", type=str, help="Domain name to be checked")
 args = parser.parse_args()
 
 # godaddy API credentials for authorization
+
 api_key = "3mM44UahMcshjE_HUPueqAPN6dU6DSVoBAsxn"
 api_secret = "QdV9FjZjKvxXmVFqfLJEMk"
 
