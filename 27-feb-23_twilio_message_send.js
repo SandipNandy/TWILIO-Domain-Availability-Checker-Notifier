a12330e8a0a09bc988b8dea1a2c09211a0ab59ea
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2026-03-11 19:07:44 +0530

    Update 27-feb-23_twilio_message_send.js

--- 27-feb-23_twilio_message_send.js
+++ 27-feb-23_twilio_message_send.js
@@ -20,11 +20,12 @@ client.messages.create({
   
   body: 'Your Twilio code is 123832', 
   
-  to: 'whatsapp:+491512828855' 
+  to: 'whatsapp:+49151828855' 
   
 }).then(msg => console.log(msg.sid));
 
 //   twilio message send.
+
 
 
 
